d9017390e6e267c8ef40db917b07c0b38ca94d94
Mikel Garcia <mikgarci@c4r2s6.42urduliz.com>
2024-01-17 17:48:25 +0100

    arreglado el /

--- src/StaticFileHTTPRequest.cpp
+++ src/StaticFileHTTPRequest.cpp
@@ -40,14 +40,17 @@ StaticFileHTTPRequest *StaticFileHTTPRequest::clone()
     return (new StaticFileHTTPRequest(*this));
 }
 
-bool esArchivo(const std::string& ruta) {
+bool esArchivo(const std::string &ruta)
+{
     struct stat info;
     return stat(ruta.c_str(), &info) == 0 && S_ISREG(info.st_mode);
 }
 
-bool esDirectorio(const std::string& ruta) {
-    DIR* directorio = opendir(ruta.c_str());
-    if (directorio != NULL) {
+bool esDirectorio(const std::string &ruta)
+{
+    DIR *directorio = opendir(ruta.c_str());
+    if (directorio != NULL)
+    {
         closedir(directorio);
         return true;
     }
@@ -57,11 +60,59 @@ bool esDirectorio(const std::string& ruta) {
 std::string getContentType(std::string str)
 {
     std::map<std::string, std::string> extensionToContentType;
-    extensionToContentType[".html"] = "text/html";
+    extensionToContentType[".aac"] = "audio/aac";
+    extensionToContentType[".abw"] = "application/x-abiword";
+    extensionToContentType[".arc"] = "application/octet-stream";
+    extensionToContentType[".avi"] = "video/x-msvideo";
+    extensionToContentType[".azw"] = "application/vnd.amazon.ebook";
+    extensionToContentType[".bin"] = "application/octet-stream";
+    extensionToContentType[".bz"] = "application/x-bzip";
+    extensionToContentType[".bz2"] = "application/x-bzip2";
+    extensionToContentType[".csh"] = "application/x-csh";
     extensionToContentType[".css"] = "text/css";
-    extensionToContentType[".js"] = "application/javascript";
+    extensionToContentType[".csv"] = "text/csv";
+    extensionToContentType[".doc"] = "application/msword";
+    extensionToContentType[".eot"] = "application/vnd.ms-fontobject";
+    extensionToContentType[".epub"] = "application/epub+zip";
+    extensionToContentType[".gif"] = "image/gif";
+    extensionToContentType[".htm"] = "text/html";
+    extensionToContentType[".html"] = "text/html";
+    extensionToContentType[".ico"] = "image/x-icon";
+    extensionToContentType[".ics"] = "text/calendar";
+    extensionToContentType[".jar"] = "application/java-archive";
+    extensionToContentType[".jpeg"] = "image/jpeg";
     extensionToContentType[".jpg"] = "image/jpeg";
+    extensionToContentType[".js"] = "application/javascript";
+    extensionToContentType[".json"] = "application/json";
+    extensionToContentType[".mid"] = "audio/midi";
+    extensionToContentType[".midi"] = "audio/midi";
+    extensionToContentType[".mpeg"] = "video/mpeg";
+    extensionToContentType[".mpkg"] = "application/vnd.apple.installer+xml";
+    extensionToContentType[".odp"] = "application/vnd.oasis.opendocument.presentation";
+    extensionToContentType[".ods"] = "application/vnd.oasis.opendocument.spreadsheet";
+    extensionToContentType[".odt"] = "application/vnd.oasis.opendocument.text";
+    extensionToContentType[".oga"] = "audio/ogg";
+    extensionToContentType[".ogv"] = "video/ogg";
+    extensionToContentType[".ogx"] = "application/ogg";
+    extensionToContentType[".otf"] = "font/otf";
     extensionToContentType[".png"] = "image/png";
+    extensionToContentType[".pdf"] = "application/pdf";
+    extensionToContentType[".ppt"] = "application/vnd.ms-powerpoint";
+    extensionToContentType[".rar"] = "application/x-rar-compressed";
+    extensionToContentType[".rtf"] = "application/rtf";
+    extensionToContentType[".sh"] = "application/x-sh";
+    extensionToContentType[".svg"] = "image/svg+xml";
+    extensionToContentType[".swf"] = "application/x-shockwave-flash";
+    extensionToContentType[".tar"] = "application/x-tar";
+    extensionToContentType[".tif"] = "image/tiff";
+    extensionToContentType[".tiff"] = "image/tiff";
+    extensionToContentType[".ts"] = "video/vnd.dlna.mpeg-tts";
+    extensionToContentType[".ttf"] = "font/ttf";
+    extensionToContentType[".vsd"] = "application/vnd.visio";
+    extensionToContentType[".wav"] = "audio/x-wav";
+    extensionToContentType[".weba"] = "audio/webm";
+    extensionToContentType[".webm"] = "video/webm";
+    extensionToContentType[".webp"] = "image/webp";
     // Extract the file extension from the filename
     size_t dotPosition = str.find_last_of(".");
     if (dotPosition != std::string::npos)
@@ -81,28 +132,31 @@ std::string getReponse(std::string path)
 {
     std::string contenido;
     int descriptorArchivo = open(path.c_str(), O_RDONLY);
-    if (descriptorArchivo == -1) {
+    if (descriptorArchivo == -1)
+    {
         throw std::runtime_error("Failed to create a listening socket");
     }
     char buffer[4096]; // Puedes ajustar el tamaño del buffer según tus necesidades
     ssize_t tmp;
     ssize_t bytesLeidos = 0;
-    while ((tmp = read(descriptorArchivo, buffer, sizeof(buffer))) > 0) {
+    while ((tmp = read(descriptorArchivo, buffer, sizeof(buffer))) > 0)
+    {
         bytesLeidos += tmp;
         contenido.append(buffer, bytesLeidos);
     }
-    if (bytesLeidos == -1) {
+    if (bytesLeidos == -1)
+    {
         throw std::runtime_error("Failed to create a listening socket");
     }
     close(descriptorArchivo);
     std::stringstream ss;
     ss << "HTTP/1.1 200 OK\r\n"
-    << "Content-Type: "
-    << getContentType(path)
-    << "\r\n"
-    << "Content-Length: " << bytesLeidos
-    << "\r\n\r\n"
-    << contenido;
+       << "Content-Type: "
+       << getContentType(path)
+       << "\r\n"
+       << "Content-Length: " << bytesLeidos
+       << "\r\n\r\n"
+       << contenido;
     return ss.str();
 }
 
@@ -110,20 +164,28 @@ HTTPResponse StaticFileHTTPRequest::process()
 {
     HTTPResponse response;
 
-// Tengo que asegurarme que al concatenar los paths, tnegan / metida entre medias.
+    // Tengo que asegurarme que al concatenar los paths, tnegan / metida entre medias.
     try
     {
-        std::string rutaCompleta = this->location.getCfgValueFrom("docroot") + this->httpHeader.getUrl();
-        if (esArchivo(rutaCompleta)) {
+        std::string dR = this->location.getCfgValueFrom("docroot");
+        std::string rQ = this->httpHeader.getUrl();
+        std::string rutaCompleta = dR + rQ;
+        if (esArchivo(rutaCompleta))
+        {
             response.setResponse(getReponse(rutaCompleta));
         }
         else if (esDirectorio(rutaCompleta))
         {
-            if (true) {//De momento entra siempre aqui, pero aqui ira la logica de autoindex
-                std::string rutaConIndex = rutaCompleta + this->location.getCfgValueFrom("index");
-                if (esArchivo(rutaConIndex))
+            size_t ultimaPosicion = rutaCompleta.find_last_of('/');
+            if (ultimaPosicion != std::string::npos && ultimaPosicion != rutaCompleta.length() - 1) {
+                rutaCompleta += "/";
+            }
+            if (true)
+            { // De momento entra siempre aqui, pero aqui ira la logica de autoindex
+                rutaCompleta += this->location.getCfgValueFrom("index");
+                if (esArchivo(rutaCompleta))
                 {
-                    response.setResponse(getReponse(rutaConIndex));
+                    response.setResponse(getReponse(rutaCompleta));
                 }
                 else
                     return HTTPResponse404();
