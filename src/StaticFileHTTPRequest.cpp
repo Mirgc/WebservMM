64100ea4aa23b9c094a0052a0554c9534b79af88
Mikel <mikgarci@student.42urduliz.com>
2024-01-24 18:46:47 +0100

    MIMETypes class created

--- src/StaticFileHTTPRequest.cpp
+++ src/StaticFileHTTPRequest.cpp
@@ -12,6 +12,7 @@
 #include "HTTPResponse.hpp"
 #include "HTTPResponse404.hpp"
 #include "HTTPResponse500.hpp"
+#include "MIMETypes.hpp"
 
 StaticFileHTTPRequest::StaticFileHTTPRequest(
     const ServerConfig &serverConfig,
@@ -57,107 +58,41 @@ bool isDirectory(const std::string &ruta)
     return false;
 }
 
-std::string getContentType(std::string str)
+std::string getResponse(const std::string& path)
 {
-    std::map<std::string, std::string> extensionToContentType;
-    extensionToContentType[".aac"] = "audio/aac";
-    extensionToContentType[".abw"] = "application/x-abiword";
-    extensionToContentType[".arc"] = "application/octet-stream";
-    extensionToContentType[".avi"] = "video/x-msvideo";
-    extensionToContentType[".azw"] = "application/vnd.amazon.ebook";
-    extensionToContentType[".bin"] = "application/octet-stream";
-    extensionToContentType[".bz"] = "application/x-bzip";
-    extensionToContentType[".bz2"] = "application/x-bzip2";
-    extensionToContentType[".csh"] = "application/x-csh";
-    extensionToContentType[".css"] = "text/css";
-    extensionToContentType[".csv"] = "text/csv";
-    extensionToContentType[".doc"] = "application/msword";
-    extensionToContentType[".eot"] = "application/vnd.ms-fontobject";
-    extensionToContentType[".epub"] = "application/epub+zip";
-    extensionToContentType[".gif"] = "image/gif";
-    extensionToContentType[".htm"] = "text/html";
-    extensionToContentType[".html"] = "text/html";
-    extensionToContentType[".ico"] = "image/x-icon";
-    extensionToContentType[".ics"] = "text/calendar";
-    extensionToContentType[".jar"] = "application/java-archive";
-    extensionToContentType[".jpeg"] = "image/jpeg";
-    extensionToContentType[".jpg"] = "image/jpeg";
-    extensionToContentType[".js"] = "application/javascript";
-    extensionToContentType[".json"] = "application/json";
-    extensionToContentType[".mid"] = "audio/midi";
-    extensionToContentType[".midi"] = "audio/midi";
-    extensionToContentType[".mpeg"] = "video/mpeg";
-    extensionToContentType[".mpkg"] = "application/vnd.apple.installer+xml";
-    extensionToContentType[".odp"] = "application/vnd.oasis.opendocument.presentation";
-    extensionToContentType[".ods"] = "application/vnd.oasis.opendocument.spreadsheet";
-    extensionToContentType[".odt"] = "application/vnd.oasis.opendocument.text";
-    extensionToContentType[".oga"] = "audio/ogg";
-    extensionToContentType[".ogv"] = "video/ogg";
-    extensionToContentType[".ogx"] = "application/ogg";
-    extensionToContentType[".otf"] = "font/otf";
-    extensionToContentType[".png"] = "image/png";
-    extensionToContentType[".pdf"] = "application/pdf";
-    extensionToContentType[".ppt"] = "application/vnd.ms-powerpoint";
-    extensionToContentType[".rar"] = "application/x-rar-compressed";
-    extensionToContentType[".rtf"] = "application/rtf";
-    extensionToContentType[".sh"] = "application/x-sh";
-    extensionToContentType[".svg"] = "image/svg+xml";
-    extensionToContentType[".swf"] = "application/x-shockwave-flash";
-    extensionToContentType[".tar"] = "application/x-tar";
-    extensionToContentType[".tif"] = "image/tiff";
-    extensionToContentType[".tiff"] = "image/tiff";
-    extensionToContentType[".ts"] = "video/vnd.dlna.mpeg-tts";
-    extensionToContentType[".ttf"] = "font/ttf";
-    extensionToContentType[".vsd"] = "application/vnd.visio";
-    extensionToContentType[".wav"] = "audio/x-wav";
-    extensionToContentType[".weba"] = "audio/webm";
-    extensionToContentType[".webm"] = "video/webm";
-    extensionToContentType[".webp"] = "image/webp";
-    // Extract the file extension from the filename
-    size_t dotPosition = str.find_last_of(".");
-    if (dotPosition != std::string::npos)
-    {
-        std::string ctExtension = str.substr(dotPosition);
-        // Look up the extension in the map
-        std::map<std::string, std::string>::iterator it = extensionToContentType.find(ctExtension);
-        if (it != extensionToContentType.end())
-        {
-            return it->second;
-        }
-    }
-    return "application/octet-stream";
-}
-
-std::string getReponse(const std::string& path)
-{
-    std::string contenido;
     int descriptorArchivo = open(path.c_str(), O_RDONLY);
-    if (descriptorArchivo == -1)
-    {
-        throw std::runtime_error("Failed to create a listening socket");
+    if (descriptorArchivo == -1) {
+        throw std::runtime_error("Failed to open the file");
     }
-    char buffer[4096]; // Puedes ajustar el tamaño del buffer según tus necesidades
-    ssize_t tmp;
-    ssize_t bytesLeidos = 0;
-    while ((tmp = read(descriptorArchivo, buffer, sizeof(buffer))) > 0)
-    {
-        bytesLeidos += tmp;
-        contenido.append(buffer, bytesLeidos);
-    }
-    if (bytesLeidos == -1)
-    {
-        throw std::runtime_error("Failed to create a listening socket");
-    }
-    close(descriptorArchivo);
+
     std::stringstream ss;
+    const int bufferSize = 4096;
+    char buffer[bufferSize];
+    ssize_t bytesLeidos;
+
+    while ((bytesLeidos = read(descriptorArchivo, buffer, sizeof(buffer))) > 0) {
+        ss.write(buffer, bytesLeidos);
+    }
+
+    if (bytesLeidos == -1) {
+        throw std::runtime_error("Failed to create a listening socket");
+    }
+
+    close(descriptorArchivo);
+
+    std::string contenido = ss.str();
+    
+    ss.str("");  // Limpiar el stringstream para reutilizarlo
     ss << "HTTP/1.1 200 OK\r\n"
        << "Content-Type: "
-       << getContentType(path)
+       << MIMETypes::getContentType(path)
        << "\r\n"
-       << "Content-Length: " << bytesLeidos
+       << "Content-Length: " << contenido.size()
        << "\r\n\r\n"
        << contenido;
+
     return ss.str();
+
 }
 
 std::string intToString(int value) {
@@ -213,7 +148,7 @@ HTTPResponse StaticFileHTTPRequest::process()
         pathComplete = this->location.getCfgValueFrom("docroot") + this->httpHeader.getUrl();
         if (isFile(pathComplete))
         {
-            response.setResponse(getReponse(pathComplete));
+            response.setResponse(getResponse(pathComplete));
         }
         else if (isDirectory(pathComplete))
         {
@@ -226,7 +161,7 @@ HTTPResponse StaticFileHTTPRequest::process()
                 pathComplete += this->location.getCfgValueFrom("index");
                 if (isFile(pathComplete))
                 {
-                    response.setResponse(getReponse(pathComplete));
+                    response.setResponse(getResponse(pathComplete));
                 }
                 else
                     return HTTPResponse404();
