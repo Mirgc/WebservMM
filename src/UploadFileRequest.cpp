e35008322b5fe5ed87f1103797e0647dc9eab3b1
Mikel Garcia <mikgarci@c4r3s6.42urduliz.com>
2024-02-03 11:49:29 +0100

    Finished UploadFile

--- src/UploadFileRequest.cpp
+++ src/UploadFileRequest.cpp
@@ -9,7 +9,6 @@
 #include <fcntl.h>
 #include <vector>
 
-
 #include "UploadFileRequest.hpp"
 #include "HTTPResponse.hpp"
 #include "HTTPResponse404.hpp"
@@ -44,15 +43,19 @@ UploadFileRequest *UploadFileRequest::clone()
     return (new UploadFileRequest(*this));
 }
 
-std::string getBoundary(const std::string& contentTypeHeader) {
+std::string getBoundary(const std::string &contentTypeHeader)
+{
     // Buscar la posición de "boundary="
     size_t posBoundary = contentTypeHeader.find("boundary=");
-    
+
     // Verificar si se encontró "boundary="
-    if (posBoundary != std::string::npos) {
+    if (posBoundary != std::string::npos)
+    {
         // Extraer la cadena después de "boundary="
         return contentTypeHeader.substr(posBoundary + 9);
-    } else {
+    }
+    else
+    {
         // "boundary=" no encontrado
         return "";
     }
@@ -61,56 +64,56 @@ std::string getBoundary(const std::string& contentTypeHeader) {
 HTTPResponse UploadFileRequest::process()
 {
     HTTPResponse response;
-    
+
     std::string boundary = this->httpHeader.getHeader("Content-Type");
     boundary = "--" + getBoundary(boundary);
     if (boundary.empty())
-        //Tiene que ser un 400
+        // Must be a 400 error
         return HTTPResponse404(this->serverConfig.get404Content());
-    // Tu cuerpo de solicitud
     std::string body = httpBody.getFullBody();
 
-    // Encuentra el límite (boundary) en el cuerpo de la solicitud
+    // Find the limit of the boundary
     size_t pos = body.find(boundary);
-    if (pos == std::string::npos) {
-        std::cerr << "No se encontró el límite (boundary) en el cuerpo de la solicitud." << std::endl;
+    if (pos == std::string::npos)
+    {
         return HTTPResponse404(this->serverConfig.get404Content());
     }
 
-    // Extrae el encabezado Content-Disposition y el nombre de archivo
+    // Extrat Content-Disposition and filename
     size_t start = body.find("Content-Disposition: ", pos);
     size_t end = body.find("\r\n\r\n", start);
-    if (start == std::string::npos || end == std::string::npos) {
-        std::cerr << "No se encontró el encabezado Content-Disposition en el cuerpo de la solicitud." << std::endl;
+    if (start == std::string::npos || end == std::string::npos)
+    {
         return HTTPResponse404(this->serverConfig.get404Content());
     }
 
     std::string contentDisposition = body.substr(start, end - start);
     size_t filenamePos = contentDisposition.find("filename=\"");
-    if (filenamePos == std::string::npos) {
-        std::cerr << "No se encontró el nombre de archivo en el encabezado Content-Disposition." << std::endl;
+    if (filenamePos == std::string::npos)
+    {
         return HTTPResponse404(this->serverConfig.get404Content());
     }
 
-    filenamePos += 10; // Longitud de "filename=\""
+    filenamePos += 10; // "filename=\" size"
     size_t filenameEnd = contentDisposition.find("\"", filenamePos);
     std::string filename = contentDisposition.substr(filenamePos, filenameEnd - filenamePos);
 
-    // Extrae el contenido del archivo
-    size_t contentStart = end + 4; // Longitud de "\r\n\r\n"
+    // Take content from the file
+    size_t contentStart = end + 4; // "\r\n\r\n" size
     size_t contentEnd = body.find(boundary, contentStart);
     std::string fileContent = body.substr(contentStart, contentEnd - contentStart);
-    filename = "./www/uploader/uploaded/" + filename; 
-    // Guarda el contenido en un archivo
-    FILE* outFile = std::fopen(filename.c_str(), "wb");
-    if (outFile == nullptr) {
-        std::cerr << "Error al abrir el archivo para escritura." << std::endl;
-        return HTTPResponse404(this->serverConfig.get404Content());
+    // Change to the specific getter
+    filename = this->location.getCfgValueFrom("upload_path") + "/" + filename;
+    // Save content in the file
+    FILE *outFile = std::fopen(filename.c_str(), "wb");
+    if (outFile == nullptr)
+    {
+        return HTTPResponse500(this->serverConfig.get500Content());
     }
 
     std::fwrite(fileContent.c_str(), 1, fileContent.size(), outFile);
     std::fclose(outFile);
-        std::string responseBody =
+    std::string responseBody =
         "<!DOCTYPE html>"
         "<html>"
         "<head>"
@@ -122,12 +125,11 @@ HTTPResponse UploadFileRequest::process()
         "</body>"
         "</html>";
 
-    // Construir la respuesta HTTP
     std::stringstream ss;
     ss << "HTTP/1.1 201 Created\r\n";
     ss << "Content-Length: " << responseBody.size() << "\r\n";
     ss << "Content-Type: text/html\r\n";
-    ss << "\r\n";  // Fin de las cabeceras, inicio del cuerpo
+    ss << "\r\n"; // End of the headders
     ss << responseBody;
     response.setResponse(ss.str());
     return (response);
